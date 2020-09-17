ddd12507476e7cfeac8a1ec0b4d7bc8b8ac35b5f
David Long <dlong@cagedata.com>
2020-09-17 11:11:43 -0400

    Updates to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,13 @@
 $ProfileRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path)
-$env:path += ";$ProfileRoot"
+$env:path += ";$ProfileRoot;$ProfileRoot/Scripts"
 
 Get-ChildItem -Path (Join-Path -Path $ProfileRoot -ChildPath "Functions/*.ps1") | ForEach-Object { . ($_.FullName)} | Out-Null
 
-Set-Location -Path $env:USERPROFILE
+# Set-Location -Path $env:USERPROFILE
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+
--- install.ps1
+++ install.ps1
@@ -4,7 +4,8 @@ $Modules = @(
   "AzureAD",
   "MSOnline",
   "Microsoft.Online.SharePoint.PowerShell",
-  "MicrosoftTeams"
+  "MicrosoftTeams",
+  "psake"
 )
 foreach ($module in $Modules) {
   Install-Module -Name $module -Scope CurrentUser -Force -AllowClobber
