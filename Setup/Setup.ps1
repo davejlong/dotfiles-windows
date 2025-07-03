3ae3c9594d8a5819eef974d127b9c58833fcbed4
David Long <dlong@cagedata.com>
2025-07-03 12:26:29 -0400

    Cleaning out old setup scripts

--- Setup/Setup.ps1
+++
@@ -1,8 +0,0 @@
-﻿$InstallerRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-. "$InstallerRoot/Install-Winget.ps1"
-
-# Make sure PowerShell 7 is installed
-. winget install "Microsoft.PowerShell" --silent --accept-package-agreements
-
-. "$InstallerRoot/Execute-Setup.ps1"
